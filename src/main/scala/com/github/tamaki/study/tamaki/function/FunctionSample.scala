16c0485d3e204ed2c9a13e78f97ebc82d2145a89
玉木 信太郎 <shintaro.tamaki@bizreach.co.jp>
2015-08-13 10:42:27 +0900

    chainFunctionまで

--- src/main/scala/com/github/tamaki/study/tamaki/function/FunctionSample.scala
+++ src/main/scala/com/github/tamaki/study/tamaki/function/FunctionSample.scala
@@ -10,7 +10,10 @@ object FunctionSample {
     println(area(3.14)(10))
     val adder31 = adder(3, _:Int)
     println(adder31(10))
-
+    println(h(10))
+    println(i(10))
+    println(f4(10))
+    println(f5(10))
   }
 
   def createAdder(x: Int): (Int) => Int = {
@@ -22,6 +25,21 @@ object FunctionSample {
   }
 
   def adder(x: Int, y: Int) = x + y
+
+  val f = (x: Int) => x + 1
+  val g = (x: Int) => x * 3
+  val h = g.compose(f)
+  val i = g.andThen(f)
+
+  val f1 = (x: Int) => x + 1
+  val f2 = (x: Int) => x * 2
+  val f3 = (x: Int) => x - 1
+  val f4 = chainFunction(f1, f2, f3)
+  val f5 = chainFunction(f3, f2, f1)
+
+  def chainFunction(functions: (Int => Int)*) = {
+    functions.reduce((x, y) => x andThen y )
+  }
 
 
 }
